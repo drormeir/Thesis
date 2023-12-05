16fab85d74d2c598beedb27b99c34cbbe6250e08
Dror Meirovich <dror@drorm.com>
2023-12-05 15:38:38 +0200

    .

--- Higher_Criticism.py
+++ Higher_Criticism.py
@@ -6,22 +6,37 @@ from Synthetic_Data_Generators import Data_Generator_Base
 from tqdm import tqdm
 
 class Higher_Criticism:
-    def __init__(self, use_import: bool, gamma: float = 0.99, correct_symetric_baseline_p_values: bool = True):
+    def __init__(self, use_import: bool, gamma: float = 1.0, correct_symetric_baseline_p_values: bool = True):
         self.use_import = use_import
-        self.correct_symetric_baseline_p_values = correct_symetric_baseline_p_values
+        self.start_baseline_index_p_values = 0.5 if correct_symetric_baseline_p_values else 1.0
         self.gamma = gamma
         self.num_rejected = 0
         self.p_threshold = 0
         self.best_objective = 0
 
+    @staticmethod
+    def monte_carlo_statistics_HC(monte_carlo: int, data_generator: Data_Generator_Base, disable_tqdm: bool = False) -> dict:
+        results = [[],[],[]]
+        hc_models = [Higher_Criticism(use_import=True), Higher_Criticism(use_import=False,correct_symetric_baseline_p_values=False),
+                     Higher_Criticism(use_import=False, correct_symetric_baseline_p_values=True)]
+        N = data_generator.N
+        for i in tqdm(range(monte_carlo), disable=disable_tqdm):
+            data_generator.generate(seed=i)
+            p_values = np.sort(data_generator.p_values)
+            for r,hc in zip(results,hc_models):
+                hc.run_sorted_p(p_values)
+                r.append(hc.best_objective)
+        return {'import_HC' : results[0],
+                'HC_1_0':results[1],
+                'HC_0_5': results[2]}
+    
     def monte_carlo_statistics(self, monte_carlo: int, data_generator: Data_Generator_Base, disable_tqdm: bool = False) -> dict:
         nums_rejected = []
         best_objectives = []
-        first_drawdown = []
         first_p_value = []
         lowest_angle = []
         N = data_generator.N
-        angle_x_axis = np.arange(start=0.5,stop=N+0.1,step=1, dtype=np.float32)
+        angle_x_axis = self.p_values_index(N)
         for i in tqdm(range(monte_carlo), disable=disable_tqdm):
             data_generator.generate(seed=i)
             p_values = np.sort(data_generator.p_values)
@@ -29,13 +44,10 @@ class Higher_Criticism:
             # collecting data from objective function
             best_objectives.append(self.best_objective)
             nums_rejected.append(self.num_rejected)
-            objectives = self.objectives
-            first_drawdown.append(objectives[0] - objectives[:self.num_rejected+1].min())
             first_p_value.append(p_values[0])
             lowest_angle.append((p_values/angle_x_axis).min())
         return {'nums_rejected' : nums_rejected,
                 'best_objectives':best_objectives,
-                'first_drawdown':first_drawdown,
                 'first_p_value': first_p_value,
                 'lowest_angle':lowest_angle}
 
@@ -43,15 +55,15 @@ class Higher_Criticism:
         self.run_sorted_p(np.sort(p_values_unsorted))
 
     def run_sorted_p(self, p_values_sorted: np.ndarray) -> None:
+        N = int(p_values_sorted.size)
+        gamma = min(self.gamma, (N-1)/N)  # avoid last element devision by zero
         if self.use_import:
             mtest = MultiTest(p_values_sorted)
-            mtest.hc(gamma=self.gamma)
-            _, self.p_threshold = self.objectives = mtest._zz
+            _, self.p_threshold = mtest.hc(gamma=gamma)
+            self.objectives = mtest._zz
             self.num_rejected = np.sum(p_values_sorted <= self.p_threshold)
         else:
-            N = int(p_values_sorted.size)
-            start = 0.5 if self.correct_symetric_baseline_p_values else 1.0
-            i_N = np.arange(start=start,stop=N+0.1,step=1, dtype=np.float32) / N
+            i_N = self.p_values_index(N) / N
             nominator = math.sqrt(N)*(i_N - p_values_sorted)
             denominator = np.sqrt(i_N*(1-i_N))
             # trimming zeros in denominator
@@ -79,6 +91,9 @@ class Higher_Criticism:
         else:
             self.p_threshold = p_values_sorted[self.num_rejected-1]
             self.best_objective = self.objectives[self.num_rejected-1]
+
+    def p_values_index(self, N: int) -> np.ndarray:
+        return np.arange(start=self.start_baseline_index_p_values,stop=N+0.1,step=1.0, dtype=np.float32)
 
     def plot_objectives(self, title: str = 'Higher Criticism objectives values'):
         hc_objectives = self.objectives
