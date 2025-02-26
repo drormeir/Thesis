cb784411494b51a75113570c98699cdc8961bae7
Dror Meirovich <dror@drorm.com>
2025-02-26 20:21:16 +0200

    test speed berk jones

--- python/adaptive_methods/adaptive_methods.py
+++ python/adaptive_methods/adaptive_methods.py
@@ -1,9 +1,45 @@
 import numpy as np
+from tqdm import tqdm
 from python.hpc import use_njit, HybridArray
 from python.adaptive_methods.numba_gpu import higher_criticism_gpu, higher_criticism_unstable_gpu, berk_jones_gpu
 from python.adaptive_methods.numba_cpu import higher_criticism_cpu_njit, higher_criticism_unstable_cpu_njit, berk_jones_cpu_njit
 from python.adaptive_methods.python_native import higher_criticism_py, higher_criticism_unstable_py, berk_jones_py
 from python.array_math_utils.array_math_utils import cumulative_argmin, cumulative_min_inplace, cumulative_dominant_argmin, cumulative_dominant_min_inplace
+from python.rare_weak_model.rare_weak_model import rare_weak_null_hypothesis
+
+
+def test_speed_transforms(\
+        N: int,\
+        num_monte: int,\
+        num_executions: int,\
+        use_gpu: bool|None=None,\
+        transform_method: str ='identity',
+        **kwargs) -> None:
+    desc = f'Test Speed Transforms {transform_method=}'
+    with HybridArray().realloc(shape=(num_monte,N), dtype=np.float64, use_gpu=use_gpu) as noise:
+        for ind_execution in tqdm(range(num_executions), desc=desc, unit="step"):
+            rare_weak_null_hypothesis(sorted_p_values_output=noise, ind_model=ind_execution, **kwargs)
+            apply_transform_method(sorted_p_values_input_output=noise,\
+                                   transform_method=transform_method,\
+                                   **kwargs)
+            pass
+        pass
+
+
+def test_speed_berk_jones(\
+        N: int,\
+        num_monte: int,\
+        num_executions: int,\
+        use_gpu: bool|None=None,\
+        **kwargs) -> None:
+    is_njit = use_njit(**kwargs)
+    desc = f'Test Speed Berk Jones {use_gpu=} use_njit={is_njit}'
+    with HybridArray().realloc(shape=(num_monte,N), dtype=np.float64, use_gpu=use_gpu) as noise:
+        for ind_execution in tqdm(range(num_executions), desc=desc, unit="step"):
+            rare_weak_null_hypothesis(sorted_p_values_output=noise, ind_model=ind_execution, **kwargs)
+            berk_jones(sorted_p_values_input_output=noise,**kwargs)
+            pass
+        pass
 
 
 def apply_transform_discovery_method(\
@@ -23,6 +59,7 @@ def apply_transform_discovery_method(\
             num_discoveries_output=num_discoveries_output,\
             **kwargs)
         
+
 def apply_transform_method(\
         sorted_p_values_input_output: HybridArray,\
         transform_method: str,\
@@ -38,6 +75,7 @@ def apply_transform_method(\
     else:
         assert False, f'{transform_method=}'
         
+
 def higher_criticism(sorted_p_values_input_output: HybridArray, **kwargs) -> None:
     if sorted_p_values_input_output.is_gpu():
         # GPU mode
@@ -49,6 +87,7 @@ def higher_criticism(sorted_p_values_input_output: HybridArray, **kwargs) -> Non
             higher_criticism_cpu_njit(sorted_p_values_input_output=sorted_p_values_input_output.numpy())
         else:
             higher_criticism_py(sorted_p_values_input_output=sorted_p_values_input_output.numpy())
+
 
 def higher_criticism_unstable(sorted_p_values_input_output: HybridArray, **kwargs) -> None:
     if sorted_p_values_input_output.is_gpu():
@@ -68,9 +107,10 @@ def berk_jones(\
         **kwargs) -> None:
     if sorted_p_values_input_output.is_gpu():
         # GPU mode
+        debug = kwargs.get('debug_berk_jones_gpu_block_size', None)
         grid_shape, block_shape =\
             sorted_p_values_input_output.gpu_grid_block2D_square_shapes(\
-                registers_per_thread=256)
+                registers_per_thread=256, debug=debug)
         berk_jones_gpu[grid_shape, block_shape](sorted_p_values_input_output.gpu_data()) # type: ignore
     else:
         # CPU mode
@@ -78,6 +118,7 @@ def berk_jones(\
             berk_jones_cpu_njit(sorted_p_values_input_output=sorted_p_values_input_output.numpy())
         else:
             berk_jones_py(sorted_p_values_input_output=sorted_p_values_input_output.numpy())
+
 
 def apply_discovery_method_on_transformation(\
         transformed_p_values_input: HybridArray,\
@@ -87,6 +128,7 @@ def apply_discovery_method_on_transformation(\
         cumulative_min_inplace(array=transformed_p_values_input, **kwargs)
     else:
         cumulative_dominant_min_inplace(array=transformed_p_values_input, **kwargs)
+
 
 def discover_by_method(\
         transformed_p_values_input: HybridArray,\
