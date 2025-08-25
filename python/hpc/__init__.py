8cbe4af67ee02068a3106b4e0f8e850f86f7f883
drormeir <dror@drorm.com>
2025-08-25 14:14:05 +0300

    exposing error raising functions in hpc module

--- python/hpc/__init__.py
+++ python/hpc/__init__.py
@@ -2,11 +2,13 @@
 High Performance Computing (HPC) module for hybrid CPU/GPU array operations.
 """
 
-from .hpc import globals, is_use_njit
+from .hpc import globals, is_use_njit, raise_cuda_not_available, raise_njit_not_available
 from .hybrid_array import HybridArray
 
 __all__ = [
     'globals',
     'is_use_njit',
+    'raise_cuda_not_available',
+    'raise_njit_not_available',
     'HybridArray',
 ]
